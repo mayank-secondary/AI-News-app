9e704a991baada54af1e7bc00efa922cfa357e00
tushaar srivastava <tushaar.sri98@gmail.com>
2022-03-27 02:11:54 +0530

    added styles

--- src/styles.js
+++ src/styles.js
@@ -69,12 +69,12 @@ export default makeStyles((theme) => ({
         },
     },
     alanLogo: {
-        height: '27vmin',
+        height: '18vmin',
         borderRadius: '15%',
         padding: '0 5%',
         margin: '3% 0',
         [theme.breakpoints.down('sm')]: {
-            height: '35vmin',
+            height: '21vmin',
         },
     },
 }));
